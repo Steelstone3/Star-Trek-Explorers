6e86c6bd813fa96abb84c7794b01bc3a24fd9aa9
Skippy <steelstone3@gmail.com>
2021-07-19 13:49:44 +0100

    Learning my way through some java stuff

---
+++ tests/com/startrekexplorerstests/game/StarshipShould.java
@@ -0,0 +1,63 @@
+package com.startrekexplorerstests.game;
+
+import org.junit.jupiter.api.Test;
+
+class StarshipShould {
+
+	@Test
+	void haveAnInitialPosition() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	void moveAroundTheWorld() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	void notWrapAtTheWorldMapLimits() {
+		//fail("Not yet implemented");
+	}
+	
+	//Later tests
+	
+	@Test
+	void enterAStarSystem() {
+		
+	}
+	
+	@Test
+	void leaveAStarSystem() {
+		
+	}
+	
+	@Test
+	void enterAPlanetarySystem() {
+		
+	}
+	
+	@Test
+	void leaveAPlanetarySystem() {
+		
+	}
+	
+	@Test
+	void enterAMoonSystem() {
+		
+	}
+	
+	@Test
+	void leaveAMoonSystem() {
+	}
+	
+	@Test
+	void enterAStarbase() {
+	}
+	
+	@Test
+	void leaveAStarbase()
+	{
+		
+	}
+	
+}
