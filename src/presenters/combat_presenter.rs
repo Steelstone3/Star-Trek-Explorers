4c43ab2baab40d03b6f4c0a17bb2d10ba53a00c8
Skippy <steelstone3@gmail.com>
2022-08-14 01:45:01 +0100

    Happy

--- src/presenters/combat_presenter.rs
+++ src/presenters/combat_presenter.rs
@@ -2,6 +2,8 @@ use crate::models::ships::ship::Ship;
 use crate::presenters::presenter::read_numeric_u32;
 use crate::presenters::presenter::write;
 
+use super::presenter::read_string;
+
 pub fn choose_hostile_target(hostiles: &mut [Ship]) -> &mut Ship {
     let mut index = 0;
 
@@ -13,9 +15,7 @@ pub fn choose_hostile_target(hostiles: &mut [Ship]) -> &mut Ship {
         index += 1;
     });
 
-    let selection = read_numeric_u32("Captain select target hostile ship", 0, 0);
-
-    hostiles.get_mut(selection as usize).unwrap()
+    select_target_ship(hostiles)
 }
 
 pub fn choose_weapon_system() -> u32 {
@@ -24,24 +24,17 @@ pub fn choose_weapon_system() -> u32 {
     read_numeric_u32("Captain fire torpedoes?", 0, 1)
 }
 
-// fn select_target_ship(hostiles: &mut [Ship]) -> &mut Ship {
-//     // let selection = read_numeric_u32(
-//     //     "Captain select target hostile ship",
-//     //     0,
-//     //     0,
-//     // );
+fn select_target_ship(hostiles: &mut [Ship]) -> &mut Ship {
+    let mut result = u32::MAX;
 
-//     write("Captain select target hostile ship".to_string());
-//     let mut result = u32::MAX;
+    while result == u32::MAX || result > hostiles.len() as u32 {
+        let input = read_string("Captain select target hostile ship");
 
-//     while result == u32::MAX || result > upper_bound || result < lower_bound {
-//         let input = read_string(message);
+        result = match input.as_str().trim().parse::<u32>() {
+            Ok(result) => result,
+            Err(_e) => u32::MAX,
+        };
+    }
 
-//         result = match input.as_str().trim().parse::<u32>() {
-//             Ok(result) => result,
-//             Err(_e) => u32::MAX,
-//         };
-//     }
-
-//         hostiles.get_mut(selection as usize).unwrap()
-// }
+    hostiles.get_mut(result as usize).unwrap()
+}
