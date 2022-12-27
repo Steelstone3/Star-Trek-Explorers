8bb363b1d2ba39ee654b1e46875db6eed9db5106
Skippy <steelstone3@gmail.com>
2022-12-27 12:27:28 +0000

    spawn a starship

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,19 @@ mod components;
 mod entities;
 mod systems;
 
-fn main() {}
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("starship_enterprise_e.png"),
+        ..default()
+    });
+}
