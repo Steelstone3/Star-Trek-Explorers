2ef4d40ddadea06b736e0dd4734a41de80f5644f
Skippy <steelstone3@gmail.com>
2023-04-06 22:25:49 +0100

    linting fix

--- src/controllers/game_state.rs
+++ src/controllers/game_state.rs
@@ -9,7 +9,8 @@ const NEW_GAME: &str = "New Game";
 const LOAD_GAME: &str = "Load Game";
 
 pub fn start_state() {
-    if main_menu() == NEW_GAME.to_string() {
+    if main_menu() == NEW_GAME
+    {
         while confirmation("\nStart New Game:") {
             let new_game = Game::new();
             let galaxy_exploration = Game::<GalaxyExploration>::from(new_game);
