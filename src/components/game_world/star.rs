27ac86c4bce2e6c247fca2cd4eb17dbc85c2ea0b
Skippy <steelstone3@gmail.com>
2023-04-09 00:36:34 +0100

    Update star.rs

--- src/components/game_world/star.rs
+++ src/components/game_world/star.rs
@@ -1,6 +1,7 @@
+use crate::systems::{planet_generation::generate_planets, random_generation::generate_seed};
+
 use super::planet::Planet;
 
-#[allow(dead_code)]
 pub struct Star {
     planets: Vec<Planet>,
 }
@@ -8,14 +9,21 @@ pub struct Star {
 impl Default for Star {
     fn default() -> Self {
         Self {
-            planets: vec![Planet::default()],
+            planets: generate_planets(generate_seed()),
         }
     }
 }
 
 #[cfg(test)]
 mod star_should {
+    use super::*;
+
     #[test]
-    #[ignore = "not implemented"]
-    fn do_something() {}
+    fn create_a_default_universe() {
+        // Given
+        let star = Star::default();
+
+        // Then
+        assert_ne!(0, star.planets.len())
+    }
 }
