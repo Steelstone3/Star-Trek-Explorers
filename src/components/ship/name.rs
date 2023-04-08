23ca0738703a1e7b3337ce092b86d5d048cf1327
Skippy <steelstone3@gmail.com>
2023-04-08 13:11:43 +0100

    a bit further

--- src/components/ship/name.rs
+++ src/components/ship/name.rs
@@ -1,2 +1,3 @@
 pub mod federation_ship_name;
 pub mod klingon_ship_name;
+pub mod faction_name;
--- src/components/ship/name/federation_ship_name.rs
+++ src/components/ship/name/federation_ship_name.rs
@@ -1,4 +1,12 @@
+use std::fmt::Display;
+
 pub enum FederationShipName {
     Enterprise,
     Defiant,
+}
+
+impl Display for FederationShipName {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        
+    }
 }
