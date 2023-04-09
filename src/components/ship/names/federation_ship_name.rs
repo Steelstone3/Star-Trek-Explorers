ba2b1970749c470e04800c787d0b2fd84c9553a5
Skippy <steelstone3@gmail.com>
2023-04-09 17:33:11 +0100

    More ship names

--- src/components/ship/names/federation_ship_name.rs
+++ src/components/ship/names/federation_ship_name.rs
@@ -1,12 +1,58 @@
-use std::fmt::Display;
-
 use rand_derive2::RandGen;
+use std::fmt::Display;
 
 #[derive(PartialEq, Debug, RandGen)]
 pub enum FederationShipName {
-    Enterprise,
-    Defiant,
+    Akira,
+    Archon,
+    Atlantis,
+    Avenger,
+    Aventine,
+    Bozeman,
+    Centaur,
     Challenger,
+    Chekov,
+    Chimera,
+    Columbia,
+    Cortez,
+    Curry,
+    Dauntless,
+    Defiant,
+    Discovery,
+    Endeavour,
+    Enterprise,
+    Excalibur,
+    Excelsio,
+    Excelsior,
+    Farragut,
+    Galaxy,
+    Grissom,
+    Hood,
+    Horizon,
+    Intrepid,
+    Lakota,
+    Lexington,
+    Majestic,
+    Melbourne,
+    Merrimack,
+    Monitor,
+    Nebula,
+    Newton,
+    Nova,
+    Odyssey,
+    Pasteur,
+    Potemkin,
+    Prometheus,
+    Proxima,
+    Relativity,
+    Saratoga,
+    Stargazer,
+    Sutherland,
+    Thunderchild,
+    Titan,
+    Valiant,
+    Voyager,
+    Yorktown,
 }
 
 impl Display for FederationShipName {
@@ -20,6 +66,147 @@ impl Display for FederationShipName {
             }
             FederationShipName::Challenger => {
                 write!(f, "Challenger")
+            }
+            FederationShipName::Voyager => {
+                write!(f, "Voyager")
+            }
+            FederationShipName::Discovery => {
+                write!(f, "Discovery")
+            }
+            FederationShipName::Excelsio => {
+                write!(f, "Excelsio")
+            }
+            FederationShipName::Prometheus => {
+                write!(f, "Prometheus")
+            }
+            FederationShipName::Titan => {
+                write!(f, "Titan")
+            }
+            FederationShipName::Stargazer => {
+                write!(f, "Stargazer")
+            }
+            FederationShipName::Hood => {
+                write!(f, "Hood")
+            }
+            FederationShipName::Atlantis => {
+                write!(f, "Atlantis")
+            }
+            FederationShipName::Aventine => {
+                write!(f, "Aventine")
+            }
+            FederationShipName::Chimera => {
+                write!(f, "Chimera")
+            }
+            FederationShipName::Columbia => {
+                write!(f, "Columbia")
+            }
+            FederationShipName::Endeavour => {
+                write!(f, "Endeavour")
+            }
+            FederationShipName::Excalibur => {
+                write!(f, "Excalibur")
+            }
+            FederationShipName::Excelsior => {
+                write!(f, "Excelsior")
+            }
+            FederationShipName::Galaxy => {
+                write!(f, "Galaxy")
+            }
+            FederationShipName::Intrepid => {
+                write!(f, "Intrepid")
+            }
+            FederationShipName::Lexington => {
+                write!(f, "Lexington")
+            }
+            FederationShipName::Melbourne => {
+                write!(f, "Melbourne")
+            }
+            FederationShipName::Nova => {
+                write!(f, "Nova")
+            }
+            FederationShipName::Odyssey => {
+                write!(f, "Odyssey")
+            }
+            FederationShipName::Sutherland => {
+                write!(f, "Sutherland")
+            }
+            FederationShipName::Thunderchild => {
+                write!(f, "Thunderchild")
+            }
+            FederationShipName::Valiant => {
+                write!(f, "Valiant")
+            }
+            FederationShipName::Yorktown => {
+                write!(f, "Yorktown")
+            }
+            FederationShipName::Akira => {
+                write!(f, "Akira")
+            }
+            FederationShipName::Archon => {
+                write!(f, "Archon")
+            }
+            FederationShipName::Avenger => {
+                write!(f, "Avenger")
+            }
+            FederationShipName::Bozeman => {
+                write!(f, "Bozeman")
+            }
+            FederationShipName::Centaur => {
+                write!(f, "Centaur")
+            }
+            FederationShipName::Chekov => {
+                write!(f, "Chekov")
+            }
+            FederationShipName::Cortez => {
+                write!(f, "Cortez")
+            }
+            FederationShipName::Curry => {
+                write!(f, "Curry")
+            }
+            FederationShipName::Dauntless => {
+                write!(f, "Dauntless")
+            }
+            FederationShipName::Farragut => {
+                write!(f, "Farragut")
+            }
+            FederationShipName::Grissom => {
+                write!(f, "Grissom")
+            }
+            FederationShipName::Horizon => {
+                write!(f, "Horizon")
+            }
+            FederationShipName::Lakota => {
+                write!(f, "Lakota")
+            }
+            FederationShipName::Majestic => {
+                write!(f, "Majestic")
+            }
+            FederationShipName::Merrimack => {
+                write!(f, "Merrimack")
+            }
+            FederationShipName::Monitor => {
+                write!(f, "Monitor")
+            }
+            FederationShipName::Nebula => {
+                write!(f, "Nebula")
+            }
+            FederationShipName::Newton => {
+                write!(f, "Newton")
+            }
+            FederationShipName::Pasteur => {
+                write!(f, "Pasteur")
+            }
+            FederationShipName::Potemkin => {
+                write!(f, "Potemkin")
+            }
+            FederationShipName::Proxima => {
+                write!(f, "Proxima")
+            }
+            FederationShipName::Relativity => {
+                write!(f, "Relativity")
+            }
+            FederationShipName::Saratoga => {
+                write!(f, "Saratoga")
             }
         }
     }
