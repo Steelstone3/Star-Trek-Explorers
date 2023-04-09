8315e9a461b5ff79c3b80040b0f3fb7085c384cc
Skippy <steelstone3@gmail.com>
2023-04-09 02:21:49 +0100

    Update world.rs

--- src/entities/world.rs
+++ src/entities/world.rs
@@ -1,13 +1,8 @@
 use crate::components::game_world::universe::Universe;
 
+#[derive(Default)]
 pub struct World {
     pub universe: Universe,
 }
 
-impl Default for World {
-    fn default() -> Self {
-        Self {
-            universe: Default::default(),
-        }
-    }
-}
+
