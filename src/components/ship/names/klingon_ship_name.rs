d3a4d4e260079e2db72ee8be05427f42627f6a8d
Skippy <steelstone3@gmail.com>
2023-04-09 17:42:18 +0100

    More ship names

--- src/components/ship/names/klingon_ship_name.rs
+++ src/components/ship/names/klingon_ship_name.rs
@@ -5,6 +5,30 @@ use std::fmt::Display;
 pub enum KlingonShipName {
     Amar,
     BMoth,
+    Brel,
+    Buruk,
+    ChTang,
+    Groth,
+    Heghta,
+    KVada,
+    KVort,
+    Ktinga,
+    Kitang,
+    Koraga,
+    MChar,
+    NeghVar,
+    Ningtao,
+    Orantho,
+    Pagh,
+    Rotarran,
+    Slivin,
+    Somraw,
+    TAcog,
+    Tagana,
+    TohKaht,
+    Voodieh,
+    Vorcha,
+    Vorn,
 }
 
 impl Display for KlingonShipName {
@@ -15,6 +39,78 @@ impl Display for KlingonShipName {
             }
             KlingonShipName::BMoth => {
                 write!(f, "B'Moth")
+            }
+            KlingonShipName::Brel => {
+                write!(f, "B'rel")
+            }
+            KlingonShipName::Buruk => {
+                write!(f, "B'rel")
+            }
+            KlingonShipName::ChTang => {
+                write!(f, "Ch'Tang")
+            }
+            KlingonShipName::Groth => {
+                write!(f, "Groth")
+            }
+            KlingonShipName::Heghta => {
+                write!(f, "Heghta")
+            }
+            KlingonShipName::KVada => {
+                write!(f, "K'Vada")
+            }
+            KlingonShipName::KVort => {
+                write!(f, "K'Vort")
+            }
+            KlingonShipName::Ktinga => {
+                write!(f, "K't'inga")
+            }
+            KlingonShipName::Kitang => {
+                write!(f, "Ki'tang")
+            }
+            KlingonShipName::Koraga => {
+                write!(f, "Koraga")
+            }
+            KlingonShipName::MChar => {
+                write!(f, "M'Char")
+            }
+            KlingonShipName::NeghVar => {
+                write!(f, "NeghVar")
+            }
+            KlingonShipName::Ningtao => {
+                write!(f, "Ning'tao")
+            }
+            KlingonShipName::Orantho => {
+                write!(f, "Orantho")
+            }
+            KlingonShipName::Pagh => {
+                write!(f, "Pagh")
+            }
+            KlingonShipName::Rotarran => {
+                write!(f, "Rotarran")
+            }
+            KlingonShipName::Slivin => {
+                write!(f, "Slivin")
+            }
+            KlingonShipName::Somraw => {
+                write!(f, "Somraw")
+            }
+            KlingonShipName::TAcog => {
+                write!(f, "T'Acog")
+            }
+            KlingonShipName::Tagana => {
+                write!(f, "Tagana")
+            }
+            KlingonShipName::TohKaht => {
+                write!(f, "Toh'Kaht")
+            }
+            KlingonShipName::Voodieh => {
+                write!(f, "Voodieh")
+            }
+            KlingonShipName::Vorcha => {
+                write!(f, "Vor'cha")
+            }
+            KlingonShipName::Vorn => {
+                write!(f, "Vorn")
             }
         }
     }
