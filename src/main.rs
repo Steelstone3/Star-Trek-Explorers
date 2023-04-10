a143e4b93f24a9d541e65f2e41c15d056f6eda62
Skippy <steelstone3@gmail.com>
2023-04-10 23:56:55 +0100

    Reset on the combat system

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use components::ship::names::faction_name::FactionName;
 use entities::game::Game;
 use systems::{random_generation::generate_seed, ship_generation::generate_ships};
 
-use crate::{entities::ships::klingon_ship::KlingonShip, systems::combat::combat_turn};
-
 mod components;
 mod entities;
 mod presenters;
@@ -33,20 +31,4 @@ fn main() {
     }
 
     println!("{}", game.world.universe);
-
-    let mut hostile = KlingonShip::default();
-
-    hostile.ship_systems = combat_turn(
-        generate_seed(),
-        game.player_ship.ship_systems.select_ship_weapon_type(),
-        game.player_ship.name.to_string(),
-        game.player_ship.class.to_string(),
-        game.player_ship.ship_identification,
-        game.player_ship.ship_systems,
-        hostile.name.to_string(),
-        hostile.class.to_string(),
-        hostile.ship_identification,
-        hostile.ship_systems,
-    );
-    hostile.ship_systems.display_defensive_capabilities();
 }
--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -1,7 +1,3 @@
-use crate::components::ship::{
-    names::ship_identification::ShipIdentification, ship_capabilities::ship_systems::ShipSystems,
-};
-
 // TODO this takes too many arguements
 // It is basically two ships,
 // Ship can be converged with the original design
@@ -9,49 +5,69 @@ use crate::components::ship::{
 // This will use one type and have comments to seperate the races in the enum
 // We will then likely have to use arrays or something like that in the original design
 // From here hopefully everything should just "work"
-pub fn combat_turn(
-    seed: u64,
-    weapon_name: String,
-    attacking_ship_name: String,
-    attacking_ship_class: String,
-    attacking_ship_identification: ShipIdentification,
-    attacking_ship_systems: ShipSystems,
-    defending_ship_name: String,
-    defending_ship_class: String,
-    defending_ship_identification: ShipIdentification,
-    mut defending_ship_systems: ShipSystems,
-) -> ShipSystems {
-    print_turn(
-        attacking_ship_identification,
-        attacking_ship_name,
-        attacking_ship_class,
-        defending_ship_identification,
-        defending_ship_name,
-        defending_ship_class,
-    );
-    let damage = attacking_ship_systems.calculate_damage_from_weapon(seed, weapon_name);
-    println!("{}", damage);
-    // TODO this isn't updating as expected
-    defending_ship_systems.take_damage_from_hostile_ship(damage);
-    defending_ship_systems
-}
+// pub fn combat_turn(
+//     seed: u64,
+//     weapon_name: String,
+//     attacking_ship_name: String,
+//     attacking_ship_class: String,
+//     attacking_ship_identification: ShipIdentification,
+//     attacking_ship_systems: ShipSystems,
+//     defending_ship_name: String,
+//     defending_ship_class: String,
+//     defending_ship_identification: ShipIdentification,
+//     defending_ship_systems: ShipSystems,
+// ) -> ShipSystems {
+//     print_turn(
+//         attacking_ship_identification,
+//         attacking_ship_name,
+//         attacking_ship_class,
+//         defending_ship_identification,
+//         defending_ship_name,
+//         defending_ship_class,
+//     );
+//     apply_damage(
+//         seed,
+//         weapon_name,
+//         attacking_ship_systems,
+//         defending_ship_systems,
+//     )
+// }
 
-fn print_turn(
-    attacking_ship_identification: ShipIdentification,
-    attacking_ship_name: String,
-    attacking_ship_class: String,
-    defending_ship_identification: ShipIdentification,
-    defending_ship_name: String,
-    defending_ship_class: String,
-) {
-    print!("Attacking Ship: ");
-    attacking_ship_identification.display_ship_name(attacking_ship_name, attacking_ship_class);
-    print!("Defending Ship: ");
-    defending_ship_identification.display_ship_name(defending_ship_name, defending_ship_class)
-}
+// fn print_turn(
+//     attacking_ship_identification: ShipIdentification,
+//     attacking_ship_name: String,
+//     attacking_ship_class: String,
+//     defending_ship_identification: ShipIdentification,
+//     defending_ship_name: String,
+//     defending_ship_class: String,
+// ) {
+//     print!("Attacking Ship: ");
+//     attacking_ship_identification.display_ship_name(attacking_ship_name, attacking_ship_class);
+//     print!("Defending Ship: ");
+//     defending_ship_identification.display_ship_name(defending_ship_name, defending_ship_class)
+// }
+
+// fn apply_damage(
+//     seed: u64,
+//     weapon_name: String,
+//     attacking_ship_systems: ShipSystems,
+//     mut defending_ship_systems: ShipSystems,
+// ) -> ShipSystems {
+//     let damage = attacking_ship_systems.calculate_damage_from_weapon(seed, weapon_name);
+//     println!("{}", damage);
+//     // TODO this isn't updating as expected
+//     defending_ship_systems.take_damage_from_hostile_ship(damage);
+//     defending_ship_systems
+// }
 
 #[cfg(test)]
 mod combat_should {
+    // use std::default;
+
+    // use super::*;
+    // use crate::entities::ships::federation_ship::FederationShip;
+    // use crate::entities::ships::klingon_ship::KlingonShip;
+    // use rstest::rstest;
 
     // #[rstest]
     // #[case(0, 91)]
@@ -59,15 +75,20 @@ mod combat_should {
     // fn be_able_to_let_plater_damage_shields(#[case] seed: u64, #[case] remaining_shield: u8) {
     //     // Given
     //     let attacking_ship = FederationShip::default();
-    //     let mut defending_ship = KlingonShip::default();
+    //     let defending_ship = KlingonShip::default();
     //     let weapon_name = attacking_ship.ship_systems.phaser.to_string();
 
     //     // When
-    //     apply_damage(seed, weapon_name, &attacking_ship, &mut defending_ship);
+    //     let defending_ship_systems = apply_damage(
+    //         seed,
+    //         weapon_name,
+    //         attacking_ship.ship_systems,
+    //         defending_ship.ship_systems,
+    //     );
 
     //     // Then
-    //     assert_eq!(remaining_shield, defending_ship.ship_systems.shield.current);
-    //     assert_eq!(100, defending_ship.ship_systems.hull.current);
+    //     assert_eq!(remaining_shield, defending_ship_systems.shield.current);
+    //     assert_eq!(100, defending_ship_systems.hull.current);
     // }
 
     // #[rstest]
@@ -75,22 +96,25 @@ mod combat_should {
     // #[case(4545, 60)]
     // fn be_able_to_let_plater_damage_hull(#[case] seed: u64, #[case] remaining_hull: u8) {
     //     // Given
-    //     let attacking_ship = FederationShip::default();
-    //     let mut defending_ship = KlingonShip::default();
+    //     let attacking_ship = FederationShip {
+    //         name:rand::random(),
+    //         class:rand::random(),
+    //         ship_identification: default(),
+    //         ship_systems: ShipSystems { shield: default(), hull: default(), phaser: Phaser{max}, torpedo: () }
+    //     };
+    //     let defending_ship = KlingonShip::default();
     //     let weapon_name = attacking_ship.ship_systems.phaser.to_string();
 
     //     // When
-    //     for _ in 0..20 {
-    //         apply_damage(
-    //             seed,
-    //             weapon_name.to_string(),
-    //             &attacking_ship,
-    //             &mut defending_ship,
-    //         );
-    //     }
+    //     let defending_ship_systems= apply_damage(
+    //         seed,
+    //         weapon_name.to_string(),
+    //         attacking_ship.ship_systems,
+    //         defending_ship.ship_systems,
+    //     );
 
     //     // Then
-    //     assert_eq!(0, defending_ship.ship_systems.shield.current);
-    //     assert_eq!(remaining_hull, defending_ship.ship_systems.hull.current);
+    //     assert_eq!(0, defending_ship_systems.shield.current);
+    //     assert_eq!(remaining_hull, defending_ship_systems.hull.current);
     // }
 }
