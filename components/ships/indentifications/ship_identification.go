fc5b5399cab3fb2ebb5ab6cacd695742fe760117
Skippy <steelstone3@gmail.com>
2023-05-11 14:26:56 +0100

    ship identification added

---
+++ components/ships/indentifications/ship_identification.go
@@ -0,0 +1,8 @@
+package indentifications
+
+type ShipIdentification struct {
+	name string
+	class string
+	serial_number string
+	faction string
+}
---
+++ components/ships/ship.go
@@ -0,0 +1,9 @@
+package ships
+
+import (
+	"github.com/Steelstone3/Star-Trek-Explorers/components/ships/indentifications"
+)
+
+type Ship struct {
+	ship_identification indentifications.ShipIdentification
+}
