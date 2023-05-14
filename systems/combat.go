cd55072a18eb4b1c5177b3f375bf5c6ada6b60cc
Skippy <steelstone3@gmail.com>
2023-05-15 00:53:58 +0100

    Update combat.go

--- systems/combat.go
+++ systems/combat.go
@@ -7,10 +7,10 @@ import (
 
 func StartCombat(game entities.Game) {
 	//TODO If Alive Klingons && Alive player then
-	//TODO Player select alive klingon vessel
+	//TODO Player select alive klingon vessel (UI console library)
 	game.KlingonShips[0] = combatTurn(game.PlayerShip, game.KlingonShips[0])
 	//TODO If Alive Klingons && Alive player then
-	//TODO AI Select alive klingon vessel to attack player
+	//TODO AI Select alive klingon vessel to attack player (random index in range from seed)
 	game.PlayerShip = combatTurn(game.KlingonShips[0], game.PlayerShip)
 
 	endCombat(game)
